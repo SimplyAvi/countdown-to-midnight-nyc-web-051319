cbd9635b6c83e0daf273d4a3accbd38f48ec9741
Avitosh Totaram <a.totaram@gmail.com>
2019-04-28 07:08:59 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  
-  while number>=0 
+  number = 10 
+  while number>=-1
   puts "#{number} SECNDS(S)!"
   number-=1 
 end
-puts "HAPPY NEW YEAR!"
+return "HAPPY NEW YEAR!"
 end
